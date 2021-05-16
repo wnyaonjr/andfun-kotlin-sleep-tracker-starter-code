c945757b67f51b3b03b0b70b65efc9d702c080be
wnyaonjr <wnyaonjr@gmail.com>
2021-05-16 14:33:30 +0800

    - Create an interface SleepDatabaseDao and annotate it with @Dao - Add an @Insert annotation, and an insert() function that takes one SleepNight - In the same way, add an @Update annotation with an update() function for one SleepNight - Add a @Query annotation with a function get() that takes a Long key argument and returns a nullable SleepNight - Add another @Query with a clear() function and a SQLite query to delete everything from the daily_sleep_quality_table - Add a @Query to getAllNights() - Add a @Query to getTonight(). Make the returned SleepNight nullable, so that it can handle if the table is empty

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,26 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao {
+    @Insert
+    suspend fun insert(night: SleepNight)
+
+    @Update
+    suspend fun update(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    suspend fun get(key: Long): SleepNight
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    suspend fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    suspend fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    suspend fun getTonight(): SleepNight?
+}
